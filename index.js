37103fde6d09e3f64f69300045f5bbd34f2852d4
Joes131205 <Joes131205@users.noreply.github.com>
2022-12-14 22:14:31 +0700

    initial

---
+++ index.js
@@ -0,0 +1,28 @@
+const daysEl = document.getElementById("days")
+const hoursEl = document.getElementById("hours")
+const minutesEl = document.getElementById("minutes")
+const secondsEl = document.getElementById("seconds")
+const messageEl = document.getElementById("message")
+
+setInterval(time = () => {
+    let endDate = new Date("January 1, 2023 00:00:00")
+    let nowDate = new Date()
+    let timeLeft = endDate.getTime() - nowDate.getTime()
+    let days = Math.floor(timeLeft / (1000 * 60 * 60 * 24))
+    let hours = Math.floor(timeLeft / (1000 * 60 * 60) % 24)
+    let minutes = Math.floor(timeLeft / (1000 * 60) % 60)
+    let seconds = Math.floor(timeLeft / 1000 % 60)
+    daysEl.textContent = days
+    hoursEl.textContent = hours
+    minutesEl.textContent = minutes
+    secondsEl.textContent = seconds
+    if (timeLeft < 0) {
+        clearInterval(time)
+        document.body.style.background = 'url("fireworks.webp")'
+        messageEl.textContent = "Happy 2023!!"
+        daysEl.textContent = "🥳"
+        hoursEl.textContent = "🥂"
+        minutesEl.textContent = "🎊"
+        secondsEl.textContent = "🎉"
+    }
+}, 1000)
